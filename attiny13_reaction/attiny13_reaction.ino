1f19cabf8a383199595c3321242d0363a2b6ab28
huscker <egostalker03@mail.ru>
2022-01-11 12:18:27 +0300

    finished project

--- attiny13_reaction/attiny13_reaction.ino
+++ attiny13_reaction/attiny13_reaction.ino
@@ -1,23 +1,29 @@
+#define F_CPU 1200000UL
+
 #include <avr/io.h>
 #include <avr/delay.h>
 
 #define DATA     PB0
 #define CLOCK    PB2
 #define LATCH    PB4
-#define BUTTON   PB1  // dont change this
+#define BUTTON   PB1  // dont change this pin
 #define MENU_BTN PB3
-#define BTN_DELAY 50
+#define BTN_DELAY 1
 #define MENU_BTN_DELAY 500
 
 uint8_t mode;
+uint8_t temp;
+uint8_t i;
 uint16_t speed;
+uint8_t shift_temp;
 uint8_t speed_profile;
 uint8_t clicked;
 uint16_t prev;
 uint16_t score;
 uint16_t temp_millis;
+uint16_t dt;
 
-void shift_register(uint8_t value) // ultra fast
+void shift_register(uint8_t value) 
 {
   for (temp = 0; temp < 8; temp++)
   {
@@ -28,91 +34,86 @@ void shift_register(uint8_t value) // ultra fast
     PORTB |= (1 << CLOCK);
     PORTB &= ~(1 << CLOCK);
   }
+  PORTB |= (1 << LATCH);
+  PORTB &= ~(1 << LATCH);
 }
-ISR(INT0_vect){ // fast as fuck
-	_delay_ms(BTN_DELAY);
-	if((PINB & (1<<INPUT))) return; // avoid noise
-    clicked = 1;
+ISR(INT0_vect) {
+  _delay_ms(BTN_DELAY);
+  if ((PINB & (1 << BUTTON))) return; // avoid noise
+  clicked = 1;
 }
 
-int main(){
-    DDRB = 0;
-    DDRB |= (1<<CLOCK) | (1<<LATCH) | (1<<DATA);
-	MCUCR &= ~(1<<ISC01 | 1 << ISC00); // low level triggers INT0
-	GIMSK |= (1<<INT0); // enable trigger
-	sei(); // enable global interrupts
-    mode = 0;
-    clicked = 0;
-	for(;;){
-        if(mode == 2){ // game menu
-            temp_millis = millis();
-            if(clicked){
-                clicked = 0;
-                if(temp_millis - prev >= speed*4 && temp_millis - prev <= speed*6){
-                    score++;
-                    prev = temp_millis;
-                }else{ // game over
-                    prev = temp_millis - speed*7;
-                }
-            }
-            // print reaction ! ! ! ! 
-            if(temp_millis - prev > speed*6){
-                while(score > 0){
-                    shift_register((score & 0b00111111) << 2);
-                    _delay_ms(1000);
-                    score /= (1<<6);
-                }
-                mode = 0;
-            }
-            continue;
+int main() {
+  DDRB |= (1 << CLOCK) | (1 << LATCH) | (1 << DATA);
+  init();
+  GIMSK |= (1 << INT0); // enable trigger
+  sei(); // enable global interrupts
+  mode = 0;
+  speed_profile = 0;
+  speed = 25 + 25 * 15;
+  clicked = 0;
+  score = 0;
+  for (;;) {
+    if (mode == 2) { // game menu
+      temp_millis = millis();
+      dt = temp_millis - prev;
+      if (clicked && (PINB & (1 << BUTTON))) {
+        clicked = 0;
+        if (dt >= speed * 4 && dt <= speed * 6) { // good
+          score++;
+          shift_register(0x00);
+          for(i = 0;i<(temp_millis & 15) ;i++){
+            _delay_ms(100);
+          }
+          prev = millis();
+          continue;
+        }else{
+          prev -= 30000;
         }
-        if(mode == 0){ // idle menu
-            if(clicked){ // start game
-                mode = 2;
-                score = 0;
-                prev = millis();
-                clicked = 0;
-            }
-            shift_register(0x0);
+      }
+      shift_temp = 0;
+      for (i = 1; i < 7; i++) {
+        if (dt < speed * i) {
+          shift_temp |= 1 << i;
         }
-        if(mode == 1){ // settings menu
-            if(clicked){
-                clicked = 0;
-                speed_profile++;
-                if(speed_profile >= 6){
-                    speed_profile = 0;
-                }
-                switch (speed_profile){
-                    case 0: // child's play (demo)
-                        speed = 300; 
-                        break;
-                    case 1: // easy
-                        speed = 200;
-                        break;
-                    case 2: // normal
-                        speed = 150;
-                        break;
-                    case 3: // hard
-                        speed = 100;
-                        break;
-                    case 4: // nearly impossible
-                        speed = 50;
-                        break;
-                    case 5: // impossible
-                        speed = 1;
-                        break;
-                }
-            }
-            shift_register(1 << (speed_profile + 2));
+      }
+      shift_register(shift_temp);
+      if (dt > speed * 6) {
+        while (score > 0) {
+          shift_register((score & 0b00111111) << 1);
+          _delay_ms(2000); // checkkkkk
+          score >>= 6;
         }
-        if(!(PINB & (1<<MENU_BTN))){
-            _delay_ms(MENU_BTN_DELAY);
-            if(!(PINB & (1<<MENU_BTN))){
-                mode++;
-                if(mode >= 1){
-                    mode = 0;
-                }
-            }
+        mode = 0;
+      }
+      continue; // menu button doesnt work
+    }
+    if (mode == 0) { // idle menu
+      shift_register(0x00);
+      if (clicked && (PINB & (1 << BUTTON))) { // start game
+        mode = 2;
+        clicked = 0;
+        prev = millis();
+      }
+    }
+    if (mode == 1) { // settings menu
+      if (clicked && (PINB & (1 << BUTTON))) { // clicked and released
+        clicked = 0;
+        speed_profile++;
+        if (speed_profile >= 6) {
+          speed_profile = 0;
         }
-	}
+        speed_profile = 5 - speed_profile;
+        speed = 25 + speed_profile * speed_profile * 15;
+        speed_profile = 5 - speed_profile;
+      }
+      shift_register(1 << (speed_profile + 1));
+    }
+    if (!(PINB & (1 << MENU_BTN))) {
+      _delay_ms(MENU_BTN_DELAY);
+      if (!(PINB & (1 << MENU_BTN))) {
+        mode ^= 1;
+      }
+    }
+  }
 }
