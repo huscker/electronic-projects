757a6f3a818634d5d8ba3d342dbb2df99a476249
huscker <egostalker03@mail.ru>
2022-02-23 18:09:52 +0300

    added prototype & needs testing

--- unfinished/attiny13_simon_says/attiny13_simon_says.ino
+++ unfinished/attiny13_simon_says/attiny13_simon_says.ino
@@ -1,11 +1,11 @@
-#include <avr/io.h>
 #include <avr/delay.h>
 #include <avr/eeprom.h>
+#include <avr/io.h>
 
-#define DATA  PB0
+#define DATA PB0
 #define CLOCK PB2
 #define LATCH PB4
-#define BUTTON PB3  // dont change this
+#define BUTTON PB3 // dont change this
 #define MAX_SIZE 20
 
 uint8_t N;
@@ -13,20 +13,17 @@ uint8_t i;
 uint8_t temp;
 uint16_t rand_num;
 uint8_t seq[MAX_SIZE]; // 4*MAX_SIZE numbers
-uint8_t active; 
+uint8_t btn;
+uint8_t active;
 /*
-mode :
-1 - 4 bit: active button
-5 - 8 bit: active led
-
 shift reg:
-1 - 4 bit: button id
-5 - 8 bit: led id
+0b00001111
+  bbbbllll
+  b - btn
+  l - led
 */
-void shift_register(uint8_t value)
-{
-  for (temp = 0; temp < 8; temp++)
-  {
+void shift_register(uint8_t value) {
+  for (temp = 0; temp < 8; temp++) {
     if (value & (1 << (7 - temp)))
       PORTB |= (1 << DATA);
     else
@@ -35,60 +32,58 @@ void shift_register(uint8_t value)
     PORTB &= ~(1 << CLOCK);
   }
 }
-ISR(INT0_vect){
-	_delay_ms(100);
-	if((PINB & (1<<INPUT))) return; // avoid noise
-    if()
+uint8_t get_num(uint8_t pos) {
+  return (seq[pos >> 2] >> ((pos & 0b11)<<1)) & 0b11; // magic
 }
-uint8_t get_num(uint8_t pos){
-    return (seq[pos/4] >> 2*(pos % 4)) & 0b11; // magic
+void set_num(uint8_t pos, uint8_t num) {
+	seq[pos>>2] &= ~(3 << ((pos & 0b11) << 1));
+	seq[pos>>2] |= (num << ((pos & 0b11) << 1));
 }
-void set_num(uint8_t pos, uint8_t num){
+void push_back() {
+  active = 0;
+  if ((N + 1 < 4 * MAX_SIZE) || ((mode & 1) == 0 && N + 1 < 2 * MAX_SIZE)) {
+    rand_num = (rand_num >> 0x01U) ^
+               (-(rand_num & 0x01U) & 0xB400U); // pseudo random gen
+    set_num(N, rand_num);
+    N++;
+  } else {
+    // overflow
+  }
+  for (i = 0; i < N; i++) {
+    shift_register(1 << (get_num(i)));
+    _delay_ms(1000);
+  }
+  shift_register(0);
+}
 
-}
-void push_back(){
-    if(((N+1 < 4*MAX_SIZE) || ((mode&1) == 0 && N+1<2*MAX_SIZE)){
-            rand_num = (rand_num >> 0x01U) ^ (-(rand_num & 0x01U) & 0xB400U);// pseudo random gen
-            set_num(N,rand_num);
-            N++;
-    }else{
-        // overflow
+int main() {
+  DDRB = 0;
+  DDRB |= (1 << CLOCK) | (1 << LATCH) | (1 << DATA);
+  init(); // можно убрать если памяти не хватит
+  active = 0; // clear everything
+  btn = 5;
+  for (;;) {
+    for (i = 0; i < 4; i++) {
+      shift_register(1 << (i + 4));
+      _delay_ms(20);
+      if (PINB & (1 << BUTTON)) {
+        _delay_ms(10);
+        if (PINB & (1 << BUTTON)) {
+          btn = i;
+        }
+      }
     }
-}
-void print_array(){
-    for(i=0;i<N;i++){
-        
+    if (btn < 5) {
+      if (get_num(active) == btn) {
+        active++;
+        if (active >= N) {
+          push_back(); // active = 0, N++
+        }
+      } else {
+        N = 0;
+        push_back(); // active = 0, N++
+      }
+      btn = 5;
     }
-    i=0;
-}
-
-int main(){
-    DDRB = 0;
-    DDRB |= (1<<CLOCK) | (1<<LATCH) | (1<<DATA);
-	MCUCR &= ~(1<<ISC01 | 1 << ISC00); // low level triggers INT0
-	GIMSK |= (1<<INT0); // enable trigger
-	sei(); // enable global interrupts
-    active = 0; // clear everything
-	for(;;){
-        //check 1 button
-        button &= 0b00001111;
-        button |= 0b10000000;
-        shift_register(button & 0b10001111);
-        // delay?
-
-        //check 2 button
-        button &= 0b00001111;
-        button |= 0b01000000;
-        shift_register(button & 0b01001111);
-
-        //check 3 button
-        button &= 0b00001111;
-        button |= 0b00100000;
-        shift_register(button & 0b00101111);
-
-        //check 4 button
-        button &= 0b00001111;
-        button |= 0b00010000;
-        shift_register(button & 0b00011111);
-	}	
+  }
 }
